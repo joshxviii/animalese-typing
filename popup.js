174a5ec92bc33f749193e80fa8220b7dfff38c54
Josh <dagexviii.dev@gmail.com>
2023-02-10 18:37:11 -0500

    Updated Directories

--- popup.js
+++ popup.js
@@ -13,26 +13,30 @@ document.addEventListener('DOMContentLoaded', function() {
 		if (chrome.extension.getBackgroundPage().soundischecked) {
 		  document.getElementById('toggle').className = 'toggled-on';
 		 document.getElementById('unchecked').checked=true;
-		 chrome.browserAction.setIcon({path: 'icon.png'});
+		 chrome.browserAction.setIcon({path: 'assests/textures/icon.png'});
 		} 
 		else {
 		  document.getElementById('toggle').className = 'toggled-off';
 		 document.getElementById('unchecked').checked=false;
-		 chrome.browserAction.setIcon({path: 'icon_off.png'});
+		 chrome.browserAction.setIcon({path: 'assests/textures/icon_off.png'});
 		}
 	  
 		chrome.storage.local.get(['gender'], function (result) {
 			if(result.gender=="female") {
-				document.getElementById('female').style.backgroundImage = "url('./female_on.png')";
-				document.getElementById('male').style.backgroundImage = "url('./male.png')";
+				document.getElementById('female').style.backgroundImage = "url('./assests/textures/female_on.png')";
+				document.getElementById('male').style.backgroundImage = "url('./assests/textures/male.png')";
 			}
-			if(result.gender=="male") {
-				document.getElementById('male').style.backgroundImage = "url('./male_on.png')";
-				document.getElementById('female').style.backgroundImage = "url('./female.png')";
+			else if(result.gender=="male") {
+				document.getElementById('male').style.backgroundImage = "url('./assests/textures/male_on.png')";
+				document.getElementById('female').style.backgroundImage = "url('./assests/textures/female.png')";
+			}
+			//If the extension is loaded for the first time default to female voice type
+			else {
+				document.getElementById('female').style.backgroundImage = "url('./assests/textures/female_on.png')";
+				document.getElementById('male').style.backgroundImage = "url('./assests/textures/male.png')";
 			}
 		});
 	} 
-
 
 
 	//Set values for anything altered on popup
@@ -41,8 +45,8 @@ document.addEventListener('DOMContentLoaded', function() {
 			vol = result.volume;
 		});
 		chrome.storage.local.set({'gender':"female"});
-			document.getElementById('female').style.backgroundImage = "url('./female_on.png')";
-			document.getElementById('male').style.backgroundImage = "url('./male.png')";
+			document.getElementById('female').style.backgroundImage = "url('./assests/textures/female_on.png')";
+			document.getElementById('male').style.backgroundImage = "url('./assests/textures/male.png')";
 			chrome.runtime.sendMessage(null);
 	});
 
@@ -51,8 +55,8 @@ document.addEventListener('DOMContentLoaded', function() {
 			vol = result.volume;
 		});
 		chrome.storage.local.set({'gender':"male"});
-			document.getElementById('male').style.backgroundImage = "url('./male_on.png')";
-			document.getElementById('female').style.backgroundImage = "url('./female.png')";
+			document.getElementById('male').style.backgroundImage = "url('./assests/textures/male_on.png')";
+			document.getElementById('female').style.backgroundImage = "url('./assests/textures/female.png')";
 			chrome.runtime.sendMessage(null);
 	});
 
@@ -62,18 +66,19 @@ document.addEventListener('DOMContentLoaded', function() {
 		});
 	});
 
+	//Turning extension on and off
 	document.getElementById('unchecked').addEventListener('change', function (e) {
 		if(document.getElementById('unchecked').checked){
 		  //toggle off
 		  document.getElementById('toggle').className = 'toggled-on';
-		  chrome.browserAction.setIcon({path: 'icon.png'});
+		  chrome.browserAction.setIcon({path: 'assests/textures/icon.png'});
 		  if (chrome.extension.getBackgroundPage()) chrome.extension.getBackgroundPage().soundischecked=true;
 		  chrome.storage.local.set({'isactive':true});
 	   }
 	   else{
 		  //toggle on
 		  document.getElementById('toggle').className = 'toggled-off';
-		  chrome.browserAction.setIcon({path: 'icon_off.png'});
+		  chrome.browserAction.setIcon({path: 'assests/textures/icon_off.png'});
 		  if (chrome.extension.getBackgroundPage()) chrome.extension.getBackgroundPage().soundischecked=false;
 		  chrome.storage.local.set({'isactive':false});
 	   }
