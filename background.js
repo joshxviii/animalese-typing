72d500bc9c85e46317793ea2b9ce71cd8977a27d
Josh <125091065+joshxviii@users.noreply.github.com>
2024-09-07 17:20:18 -0400

    added sfx audio

--- background.js
+++ background.js
@@ -7,15 +7,16 @@
 
 console.log("animalese typing start");
 
-const file_type = ".aac"
+const file_type = ".aac";
 
 //Assign variables that dont exsist
-chrome.storage.local.get(['gender', 'voice_type', 'volume', 'f_voice', 'm_voice', 'isactive'], async function (result) {
+chrome.storage.local.get(['gender', 'voice_type', 'volume', 'f_voice', 'm_voice', 'sound_config', 'isactive'], async function (result) {
 	if (typeof result.isactive === 'undefined') chrome.storage.local.set({'isactive':true});
 	if (typeof result.voice_type === 'undefined') chrome.storage.local.set({'voice_type':"voice_1"});
 	if (typeof result.f_voice === 'undefined') chrome.storage.local.set({'f_voice':"voice_1",'m_voice':"voice_1"});
 	if (typeof result.gender === 'undefined') chrome.storage.local.set({'gender':"female"});
 	if (typeof result.volume === 'undefined') chrome.storage.local.set({'volume':0.5});
+	if (typeof result.sound_config === 'undefined') chrome.storage.local.set({'sound_config':0});
 	if (typeof soundischecked === 'undefined') soundischecked = true;
 
 	if (typeof result.isactive !== 'boolean') result.isactive = true;
@@ -29,63 +30,83 @@ chrome.storage.local.get(['gender', 'voice_type', 'volume', 'f_voice', 'm_voice'
 
 //Listen for inputs
 chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
-	await chrome.storage.local.get(['gender', 'voice_type', 'volume', 'f_voice', 'm_voice', 'isactive'], async function (result) {
+	await chrome.storage.local.get(['gender', 'voice_type', 'volume', 'f_voice', 'm_voice', 'sound_config', 'isactive'], async function (result) {
 
 		vol = result.volume;
 		v_type = result.voice_type;
 		g_type = result.gender;
-
+		config = result.sound_config;
 		soundischecked = result.isactive;
 
 		if(request.type == 'type') {
-			if (true) {
-				ready_audio_lists();
-				//Play sound when typing when audio.html is loaded
-				if (soundischecked) {
-					if (request.ok) {
-						play_audio(request.ok, 0.6);
-					}
+			ready_audio_lists();
+			//Play sound when typing when audio.html is loaded
+			if (soundischecked) {
+				if (config!=2 && request.ok) {
+					play_audio(request.ok+file_type, 0.6);
+				}
+				if (input_type == 'password') { //do not play animalese if password field is focused
+					play_audio(audio_special["default"], 0.2, 0.4);
+				}
+				else {
 					var keycode = request.keycode;
 					var key = request.key;
 					var input_type = request.input_type;
-					if (input_type == 'password') { //do not play animalese if password field is focused
-						play_audio(audio_back, 0.4);
-						//play_audio(audio_animalese[randomPlay(0,25)], 0.6)
-					}
-					else {
-						switch (true) {
-							case (keycode == 8):
-								play_audio(audio_back, 0.6);
-								break;
-		
-							case (key == '!'):
-								play_audio(audio_gwah, 0.6);
+					switch (true) {
+						case (keycode == 16 || keycode == 32):
+							break;
+						case (config!=1 && keycode == 8):
+							play_audio(audio_special['back'], 0.6)
+							break;
+						case (config!=1 && keycode == 13):
+							play_audio(audio_special['enter'], 0.6)
+							break;
+						case (config!=1 && keycode == 9):
+							play_audio(audio_special['tab'], 0.6)
+							break;
+						case (key == '?'):
+							if (config!=2) play_audio(audio_deksa, 0.6);
+							if (config!=1) play_audio(audio_special[key], 0.6)
+							break;
+						case (key == '!'):
+							if (config!=2) play_audio(audio_gwah, 0.6);
+							if (config!=1) play_audio(audio_special[key], 0.6)
+							break;
+						case (config!=1 && key == '~'): play_audio(audio_special[key], 0.6); break;
+						case (config!=1 && key == '@'): play_audio(audio_special[key], 0.6); break;
+						case (config!=1 && key == '#'): play_audio(audio_special[key], 0.6); break;
+						case (config!=1 && key == '$'): play_audio(audio_special[key], 0.6); break;
+						case (config!=1 && key == '%'): play_audio(audio_special[key], 0.6); break;
+						case (config!=1 && key == '^'): play_audio(audio_special[key], 0.6); break;
+						case (config!=1 && key == '&'): play_audio(audio_special[key], 0.6); break;
+						case (config!=1 && key == '*'): play_audio(audio_special[key], 0.6); break;
+						case (config!=1 && key == '('): play_audio(audio_special[key], 0.6); break;
+						case (config!=1 && key == ')'): play_audio(audio_special[key], 0.6); break;
+						case (config!=1 && key == '['): play_audio(audio_special[key], 0.6); break;
+						case (config!=1 && key == ']'): play_audio(audio_special[key], 0.6); break;
+						case (config!=1 && key == '{'): play_audio(audio_special[key], 0.6); break;
+						case (config!=1 && key == '}'): play_audio(audio_special[key], 0.6); break;
 
-							case (parseInt(key) >= 0 && parseInt(key) <= 9):
-								play_audio(audio_vocals[parseInt(key)], 1.0);
-								break;
-		
-							case (keycode == 187 || key == '+'):
-								play_audio(audio_vocals[11], 1.0);
-								break;
-		
-							case (keycode == 189 || key == '-'):
-								play_audio(audio_vocals[10], 1.0);
-								break;
-		
-							case (keycode >= 65 && keycode <= 90):
-								play_audio(audio_animalese[keycode - 65], 0.6, true);
-								break;
-		
-							case (keycode == 191):
-								if (key == '?') {
-									play_audio(audio_deksa, 1.0);
-								}
-								break;
-		
-							default:
-								break;
-						}
+						case (config!=2 && parseInt(key) >= 1 && parseInt(key) <= 9):
+							play_audio(audio_vocals[parseInt(key)-1], 1.0);
+							break;
+						case (config!=2 && parseInt(key) == 0):
+							play_audio(audio_vocals[9], 1.0);
+							break;
+						case (config!=2 && keycode == 173):
+							play_audio(audio_vocals[10], 1.0);
+							break;
+						case (config!=2 && keycode == 61):
+							play_audio(audio_vocals[11], 1.0);
+							break;
+	
+						case (config!=2 && keycode >= 65 && keycode <= 90):
+							play_audio(audio_animalese[keycode - 65], 0.6, 0.2);
+							break;
+	
+						default:
+							play_audio(config!=1 && audio_special["default"], 0.4, 0.4);
+							break;
 					}
 				}
 			}
@@ -93,10 +114,6 @@ chrome.runtime.onMessage.addListener(async function (request, sender, sendRespon
 	});
 });
 //End
-
-function randomPlay(min, max) {
-	return Math.floor(Math.random() * (max - min) + min);
-}
 
 function ready_audio_lists() {
 	//Store sound files
@@ -144,31 +161,52 @@ function ready_audio_lists() {
 	];
 	audio_deksa = 'assets/audio/animalese/'+g_type+'/'+v_type+'/Deska'+file_type;
 	audio_gwah = 'assets/audio/animalese/'+g_type+'/'+v_type+'/Gwah'+file_type;
-	audio_back = 'assets/audio/sfx/backspace'+file_type;
+	audio_special = {
+		"default": 'assets/audio/sfx/default'+file_type,
+		"back": 'assets/audio/sfx/backspace'+file_type,
+		"enter": 'assets/audio/sfx/enter'+file_type,
+		"tab": 'assets/audio/sfx/tab'+file_type,
+		"?": 'assets/audio/sfx/question'+file_type,
+		"~": 'assets/audio/sfx/tilde'+file_type,
+		"!": 'assets/audio/sfx/exclamation'+file_type,
+		"@": 'assets/audio/sfx/at'+file_type,
+		"#": 'assets/audio/sfx/pound'+file_type,
+		"$": 'assets/audio/sfx/dollar'+file_type,
+		"%": 'assets/audio/sfx/percent'+file_type,
+		"^": 'assets/audio/sfx/caret'+file_type,
+		"&": 'assets/audio/sfx/ampersand'+file_type,
+		"*": 'assets/audio/sfx/asterisk'+file_type,
+		"(": 'assets/audio/sfx/parenthesis_open'+file_type,
+		")": 'assets/audio/sfx/parenthesis_closed'+file_type,
+		"[": 'assets/audio/sfx/bracket_open'+file_type,
+		"]": 'assets/audio/sfx/bracket_closed'+file_type,
+		"{": 'assets/audio/sfx/brace_open'+file_type,
+		"}": 'assets/audio/sfx/brace_closed'+file_type
+	}
 }
-
 
 let audioCtx;
 let gainNode;
 let buffer;
 let source;
-
-async function play_audio(audio_path, volume) {
+async function play_audio(audio_path, volume, random_pitch=0.0) {
 
 	if (!audioCtx) {
 		audioCtx = new AudioContext();
-		gainNode = audioCtx.createGain();
 	}
 
 	const response = await fetch(audio_path);
 	buffer = await audioCtx.decodeAudioData(await response.arrayBuffer());
 
-	gainNode.gain.value = volume * vol;
+	gainNode = audioCtx.createGain();
+	gainNode.gain.value = volume * vol * 0.8;
 	gainNode.connect(audioCtx.destination);
 
 	source = audioCtx.createBufferSource();
 	source.connect(gainNode);
 	source.buffer = buffer;
+
+	if(random_pitch!=0) source.detune.value = (Math.random() * (300 + 300) - 300)*random_pitch;
 
 	source.start();
 }
--- popup.js
+++ popup.js
@@ -40,15 +40,19 @@ function updateList() {
 function say_OK() {
 	chrome.storage.local.get('gender', function (result) {
 		chrome.storage.local.set({'voice_type' : document.getElementById('v_type').value});
-		chrome.runtime.sendMessage({type: 'type', ok: 'assets/audio/animalese/'+result.gender+'/'+document.getElementById('v_type').value+'/OK.aac'});
+		chrome.runtime.sendMessage({type: 'type', ok: 'assets/audio/animalese/'+result.gender+'/'+document.getElementById('v_type').value+'/OK'});
 	});
 }
+function say_Gwah() {
+	chrome.runtime.sendMessage({type: 'type', key: '!'});
+}
+
 
 //Execute when popup is loaded
 document.addEventListener('DOMContentLoaded', function() {
 	updateList();
 	document.getElementById('version').innerText = "v" + chrome.runtime.getManifest().version
-	//Get saved values for everything on popup
+	//Get saved values for everything on popup==================================================================
 	chrome.storage.local.get(['isactive'], function (result) {
 		if (result.isactive==true) {
 			document.getElementById('unchecked').checked=false;
@@ -79,70 +83,78 @@ document.addEventListener('DOMContentLoaded', function() {
 				document.getElementById('female').style.backgroundImage = "url('./assets/images/female.png')";
 			}
 	});
+	chrome.storage.local.get(['sound_config'], function (result) {
+		document.getElementById('sound_config').value = result.sound_config;
+	});
 
-//Set values for anything altered on popup
-document.getElementById('female').addEventListener('click', function (e) {
-	chrome.storage.local.get(['volume'], function (result) {
-		vol = result.volume;
+
+	//Set values for anything altered on popup=================================================================
+	document.getElementById('female').addEventListener('click', function (e) {
+		chrome.storage.local.get(['volume'], function (result) {
+			vol = result.volume;
+		});
+		chrome.storage.local.get(['f_voice'], function (result) {
+			if(result.f_voice) document.getElementById('v_type').value = result.f_voice;
+		});
+		chrome.storage.local.set({'gender':"female"});
+		document.getElementById('v_type').className = 'voice_f';
+		document.getElementById('status').className = 'background_f';
+		document.getElementById('female').style.backgroundImage = "url('./assets/images/female_on.png')";
+		document.getElementById('male').style.backgroundImage = "url('./assets/images/male.png')";
+		updateList();
+		say_OK()
 	});
-	chrome.storage.local.get(['f_voice'], function (result) {
-		if(result.f_voice) document.getElementById('v_type').value = result.f_voice;
+	document.getElementById('male').addEventListener('click', function (e) {
+		chrome.storage.local.get(['volume'], function (result) {
+			vol = result.volume;
+		});
+		chrome.storage.local.get(['m_voice', 'gender'], function (result) {
+			if(result.m_voice) document.getElementById('v_type').value = result.m_voice;
+		});
+		chrome.storage.local.set({'gender':"male"});
+		document.getElementById('v_type').className = 'voice_m';
+		document.getElementById('status').className = 'background_m';
+		document.getElementById('male').style.backgroundImage = "url('./assets/images/male_on.png')";
+		document.getElementById('female').style.backgroundImage = "url('./assets/images/female.png')";
+		updateList();
+		say_OK()
 	});
-	chrome.storage.local.set({'gender':"female"});
-	document.getElementById('v_type').className = 'voice_f';
-	document.getElementById('status').className = 'background_f';
-	document.getElementById('female').style.backgroundImage = "url('./assets/images/female_on.png')";
-	document.getElementById('male').style.backgroundImage = "url('./assets/images/male.png')";
-	updateList();
-	say_OK()
-});
-document.getElementById('male').addEventListener('click', function (e) {
-	chrome.storage.local.get(['volume'], function (result) {
-		vol = result.volume;
+	document.getElementById('volume').addEventListener('input', function (e) {
+		chrome.storage.local.set({'volume' : document.getElementById('volume').value}, function () {
+			document.getElementById('vol_percent').className = 'vol_active'
+			document.getElementById('vol_percent').innerText = String( parseInt(document.getElementById('volume').value * 100) ) + "%"
+		});
 	});
-	chrome.storage.local.get(['m_voice', 'gender'], function (result) {
-		if(result.m_voice) document.getElementById('v_type').value = result.m_voice;
+	document.getElementById('volume').addEventListener('mouseup', function (e) {
+		document.getElementById('vol_percent').className = 'vol_inactive'
 	});
-	chrome.storage.local.set({'gender':"male"});
-	document.getElementById('v_type').className = 'voice_m';
-	document.getElementById('status').className = 'background_m';
-	document.getElementById('male').style.backgroundImage = "url('./assets/images/male_on.png')";
-	document.getElementById('female').style.backgroundImage = "url('./assets/images/female.png')";
-	updateList();
-	say_OK()
-});
-document.getElementById('volume').addEventListener('input', function (e) {
-	chrome.storage.local.set({'volume' : document.getElementById('volume').value}, function () {
-		document.getElementById('vol_percent').className = 'vol_active'
-		document.getElementById('vol_percent').innerText = String( parseInt(document.getElementById('volume').value * 100) ) + "%"
+	document.getElementById('v_type').addEventListener('change', function (e) {
+		chrome.storage.local.get(['gender'], function (result) {
+			if (result.gender == "female") chrome.storage.local.set({'f_voice' : document.getElementById('v_type').value});
+			if (result.gender == "male") chrome.storage.local.set({'m_voice' : document.getElementById('v_type').value});
+		});
+		chrome.storage.local.set({'voice_type' : document.getElementById('v_type').value});
+		say_OK()
 	});
-});
-document.getElementById('volume').addEventListener('mouseup', function (e) {
-	document.getElementById('vol_percent').className = 'vol_inactive'
-});
-document.getElementById('v_type').addEventListener('change', function (e) {
-	chrome.storage.local.get(['gender'], function (result) {
-		if (result.gender == "female") chrome.storage.local.set({'f_voice' : document.getElementById('v_type').value});
-		if (result.gender == "male") chrome.storage.local.set({'m_voice' : document.getElementById('v_type').value});
+
+	document.getElementById('sound_config').addEventListener('change', function (e) {
+		chrome.storage.local.set({'sound_config' : document.getElementById('sound_config').value});
+		say_Gwah()
 	});
-		chrome.storage.local.set({'voice_type' : document.getElementById('v_type').value}, function () {
-		
+
+	document.getElementById('unchecked').addEventListener('change', function (e) {
+		if(document.getElementById('unchecked').checked){
+			//toggle off
+			disable()
+			if (chrome.extension.getBackgroundPage()) chrome.extension.getBackgroundPage().soundischecked=false;
+			chrome.storage.local.set({'isactive':false});
+		}
+		else{
+			//toggle on
+			enable()
+			if (chrome.extension.getBackgroundPage()) chrome.extension.getBackgroundPage().soundischecked=true;
+			chrome.storage.local.set({'isactive':true});
+		}
 	});
-	say_OK()
-});
-document.getElementById('unchecked').addEventListener('change', function (e) {
-	if(document.getElementById('unchecked').checked){
-		//toggle off
-		disable()
-		if (chrome.extension.getBackgroundPage()) chrome.extension.getBackgroundPage().soundischecked=false;
-		chrome.storage.local.set({'isactive':false});
-	}
-	else{
-		//toggle on
-		enable()
-		if (chrome.extension.getBackgroundPage()) chrome.extension.getBackgroundPage().soundischecked=true;
-		chrome.storage.local.set({'isactive':true});
-	}
-});
 });
 //End
