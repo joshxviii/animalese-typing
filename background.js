e442dc8d23658049178df87649e706543c10c5f0
Josh <125091065+joshxviii@users.noreply.github.com>
2024-09-08 20:25:31 -0400

    Caps lock variation, arrow key noise. changed to C scale.

--- background.js
+++ background.js
@@ -8,6 +8,7 @@
 console.log("animalese typing start");
 
 const file_type = ".aac";
+const isUpperCase = str => str === str.toUpperCase();
 
 //Assign variables that dont exsist
 chrome.storage.local.get(['gender', 'voice_type', 'volume', 'f_voice', 'm_voice', 'sound_config', 'isactive'], async function (result) {
@@ -43,34 +44,39 @@ chrome.runtime.onMessage.addListener(async function (request, sender, sendRespon
 			//Play sound when typing when audio.html is loaded
 			if (soundischecked) {
 				if (config!=2 && request.ok) {
-					play_audio(request.ok+file_type, 0.6);
+					play_audio(request.ok+file_type, 0.6, 0.1, 0.0, 2);
+					return;
 				}
 				var input_type = request.input_type;
 				if (input_type == 'password') { //do not play animalese if password field is focused
 					play_audio(audio_special["default"], 0.2, 0.4);
+					return;
 				}
 				else {
 					var keycode = request.keycode;
 					var key = request.key;
 					switch (true) {
-						case (keycode == 16 || keycode == 32 || keycode == 20 || keycode == 18):
+						case (keycode == 16 || keycode == 32 || keycode == 20 || keycode == 18)://spacebar, shift, caps
 							break;
-						case (config!=1 && keycode == 8):
+						case (config!=1 && keycode >= 37 && keycode <= 40)://arrow keys
+							play_audio(audio_arrows[keycode-37], 0.5);
+							break;
+						case (config!=1 && keycode == 8)://backspace
 							play_audio(audio_special['back'], 0.6)
 							break;
-						case (config!=1 && keycode == 13):
+						case (config!=1 && keycode == 13)://enter
 							play_audio(audio_special['enter'], 0.6)
 							break;
-						case (config!=1 && keycode == 9):
+						case (config!=1 && keycode == 9)://tab
 							play_audio(audio_special['tab'], 0.6)
 							break;
 						case (key == '?'):
-							if (config!=2) play_audio(audio_deksa, 0.6);
 							if (config!=1) play_audio(audio_special[key], 0.6)
+							if (config!=2) play_audio(audio_deksa, 0.6, 0.2);
 							break;
 						case (key == '!'):
-							if (config!=2) play_audio(audio_gwah, 0.6);
 							if (config!=1) play_audio(audio_special[key], 0.6)
+							if (config!=2) play_audio(audio_gwah, 0.6, 0.2);
 							break;
 						case (config!=1 && key == '~'): play_audio(audio_special[key], 0.6); break;
 						case (config!=1 && key == '@'): play_audio(audio_special[key], 0.6); break;
@@ -101,10 +107,13 @@ chrome.runtime.onMessage.addListener(async function (request, sender, sendRespon
 							break;
 	
 						case (config!=2 && keycode >= 65 && keycode <= 90):
-							play_audio(audio_animalese[keycode - 65], 0.6, 0.2);
+							//When typing in caps have a slighty higher and louder pitch with more variation
+							if (isUpperCase(key)) play_audio(audio_animalese[keycode - 65], 0.7, 0.35, 1.6, 1);
+							else play_audio(audio_animalese[keycode - 65], 0.5, 0.2, 0, 1);
 							break;
 	
 						default:
+							//Default sound to play
 							play_audio(config!=1 && audio_special["default"], 0.4, 0.4);
 							break;
 					}
@@ -115,6 +124,7 @@ chrome.runtime.onMessage.addListener(async function (request, sender, sendRespon
 });
 //End
 
+//get audio maps ready
 function ready_audio_lists() {
 	//Store sound files
 	audio_animalese = [
@@ -159,6 +169,12 @@ function ready_audio_lists() {
 		'assets/audio/vocals/'+g_type+'/'+v_type+'/10'+file_type,
 		'assets/audio/vocals/'+g_type+'/'+v_type+'/11'+file_type
 	];
+	audio_arrows = [
+		'assets/audio/sfx/left'+file_type,
+		'assets/audio/sfx/up'+file_type,
+		'assets/audio/sfx/right'+file_type,
+		'assets/audio/sfx/down'+file_type
+	];
 	audio_deksa = 'assets/audio/animalese/'+g_type+'/'+v_type+'/Deska'+file_type;
 	audio_gwah = 'assets/audio/animalese/'+g_type+'/'+v_type+'/Gwah'+file_type;
 	audio_special = {
@@ -189,24 +205,33 @@ let audioCtx;
 let gainNode;
 let buffer;
 let source;
-async function play_audio(audio_path, volume, random_pitch=0.0) {
+async function play_audio(audio_path, volume, random_pitch=0.0, pitch=0.0, cutoff_channel=0) {
 
-	if (!audioCtx) {
-		audioCtx = new AudioContext();
-	}
+	if (!audioCtx) audioCtx = new AudioContext();
 
 	const response = await fetch(audio_path);
 	buffer = await audioCtx.decodeAudioData(await response.arrayBuffer());
 
+	//allow sounds on the same channel to cut eachother off.
+	if (source && cutoff_channel!=0) {
+		if (source.cutoff_channel == cutoff_channel) {
+			gainNode.gain.setValueAtTime(gainNode.gain.value, audioCtx.currentTime); 
+			gainNode.gain.exponentialRampToValueAtTime(0.0005, audioCtx.currentTime + 0.075);
+		}
+	}
+
+	//apply volume gain
 	gainNode = audioCtx.createGain();
-	gainNode.gain.value = volume * vol * 0.8;
+	gainNode.gain.value = volume * vol * 0.9;
 	gainNode.connect(audioCtx.destination);
 
 	source = audioCtx.createBufferSource();
 	source.connect(gainNode);
 	source.buffer = buffer;
+	source.cutoff_channel = cutoff_channel
 
-	if(random_pitch!=0) source.detune.value = (Math.random() * (300 + 300) - 300)*random_pitch;
+	//apply pitch variation and pitch shift
+	if( !(random_pitch==0 && pitch==0) ) source.detune.value = (pitch*100) + ((Math.random() * (300 + 300) - 300)*random_pitch);
 
 	source.start();
 }
