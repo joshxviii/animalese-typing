efbf93c4339476ecfbc719fbef37b0e5253d30ab
Josh <125091065+joshxviii@users.noreply.github.com>
2024-09-12 08:10:57 -0400

    Fixed disable button

--- background.js
+++ background.js
@@ -24,7 +24,7 @@ var config=0;
 var soundischecked=true;
 var sound_profile = new AnimaleseSoundProfile();
 chrome.storage.local.get(['gender', 'voice_type', 'volume', 'f_voice', 'm_voice', 'sound_config', 'sound_profile', 'isactive'], async function (result) {
-	if (typeof result.isactive === 'undefined') {chrome.storage.local.set({'isactive':true}); }
+	if (typeof result.isactive === 'undefined') {chrome.storage.local.set({'isactive':soundischecked}); }
 	if (typeof result.voice_type === 'undefined') {chrome.storage.local.set({'voice_type':v_type});}
 	if (typeof result.f_voice === 'undefined') {chrome.storage.local.set({'f_voice':v_type,'m_voice':v_type});}
 	if (typeof result.gender === 'undefined') {chrome.storage.local.set({'gender':g_type});}
@@ -60,6 +60,7 @@ async function update_values() {
 		sound_profile = result.sound_profile;
 		soundischecked = result.isactive;
 	});
+	
 	update_paths();
 }
 
@@ -70,82 +71,84 @@ chrome.runtime.onMessage.addListener(async function (request, sender, sendRespon
 				await update_values();
 			break;
 			case 'type':
-				let input_type = request.input_type;
-				if (request.config) config = request.config;
-				if (request.g_type) {
-					g_type = request.g_type; 
-					v_type = request.v_type;
-					update_paths()
-				}
-				if (input_type == 'password') { //do not play animalese if password field is focused
-					send_audio(audio_special["default"], 0.2, 0.4);
-					return;
-				}
-				else {
-					let keycode = request.keycode;
-					let key = request.key;
-					switch (true) {
-						case (keycode == 16 || keycode == 32 || keycode == 20 || keycode == 18):break;//spacebar, shift, caps
-							
-						case (config!=1 && key.startsWith("Arrow"))://arrow keys
-							send_audio(audio_arrows[(keycode-37)%4], 0.4);
-						break;
-						case (config!=1 && key == "Backspace" || key == "Delete" )://backspace, delete
-							send_audio(audio_special['back'], 1.0)
-						break;
-						case (config!=1 && key == "Enter")://enter
-							send_audio(audio_special['enter'], 0.2)
-						break;
-						case (config!=1 && key == "Tab")://tab
-							send_audio(audio_special['tab'], 0.5)
-						break;
-						case (key == '?'):
-							if (config!=1) send_audio(audio_special[key], 0.6)
-							if (config!=2) send_audio(audio_deksa, 0.6, 0.2, 0.0, 1, true);
-						break;
-						case (key == '!'):
-							if (config!=1) send_audio(audio_special[key], 0.6)
-							if (config!=2) send_audio(audio_gwah, 0.6, 0.2, 0.0, 1, true);
-						break;
-						case (config!=1 && key == '~'): send_audio(audio_special[key], 0.6); break;
-						case (config!=1 && key == '@'): send_audio(audio_special[key], 0.6); break;
-						case (config!=1 && key == '#'): send_audio(audio_special[key], 0.6); break;
-						case (config!=1 && key == '$'): send_audio(audio_special[key], 0.6); break;
-						case (config!=1 && key == '%'): send_audio(audio_special[key], 0.6); break;
-						case (config!=1 && key == '^'): send_audio(audio_special[key], 0.6); break;
-						case (config!=1 && key == '&'): send_audio(audio_special[key], 0.6); break;
-						case (config!=1 && key == '*'): send_audio(audio_special[key], 0.6); break;
-						case (config!=1 && key == '('): send_audio(audio_special[key], 0.6); break;
-						case (config!=1 && key == ')'): send_audio(audio_special[key], 0.6); break;
-						case (config!=1 && key == '['): send_audio(audio_special[key], 0.6); break;
-						case (config!=1 && key == ']'): send_audio(audio_special[key], 0.6); break;
-						case (config!=1 && key == '{'): send_audio(audio_special[key], 0.6); break;
-						case (config!=1 && key == '}'): send_audio(audio_special[key], 0.6); break;
+				if (soundischecked) {
+					let input_type = request.input_type;
+					if (request.config) config = request.config;
+					if (request.g_type) {
+						g_type = request.g_type; 
+						v_type = request.v_type;
+						update_paths()
+					}
+					if (input_type == 'password') { //do not play animalese if password field is focused
+						send_audio(audio_special["default"], 0.2, 0.4);
+						return;
+					}
+					else {
+						let keycode = request.keycode;
+						let key = request.key;
+						switch (true) {
+							case (keycode == 16 || keycode == 32 || keycode == 20 || keycode == 18):break;//spacebar, shift, caps
+								
+							case (config!=1 && key.startsWith("Arrow"))://arrow keys
+								send_audio(audio_arrows[(keycode-37)%4], 0.4);
+							break;
+							case (config!=1 && key == "Backspace" || key == "Delete" )://backspace, delete
+								send_audio(audio_special['back'], 1.0)
+							break;
+							case (config!=1 && key == "Enter")://enter
+								send_audio(audio_special['enter'], 0.2)
+							break;
+							case (config!=1 && key == "Tab")://tab
+								send_audio(audio_special['tab'], 0.5)
+							break;
+							case (key == '?'):
+								if (config!=1) send_audio(audio_special[key], 0.6)
+								if (config!=2) send_audio(audio_deksa, 0.6, 0.2, 0.0, 1, true);
+							break;
+							case (key == '!'):
+								if (config!=1) send_audio(audio_special[key], 0.6)
+								if (config!=2) send_audio(audio_gwah, 0.6, 0.2, 0.0, 1, true);
+							break;
+							case (config!=1 && key == '~'): send_audio(audio_special[key], 0.6); break;
+							case (config!=1 && key == '@'): send_audio(audio_special[key], 0.6); break;
+							case (config!=1 && key == '#'): send_audio(audio_special[key], 0.6); break;
+							case (config!=1 && key == '$'): send_audio(audio_special[key], 0.6); break;
+							case (config!=1 && key == '%'): send_audio(audio_special[key], 0.6); break;
+							case (config!=1 && key == '^'): send_audio(audio_special[key], 0.6); break;
+							case (config!=1 && key == '&'): send_audio(audio_special[key], 0.6); break;
+							case (config!=1 && key == '*'): send_audio(audio_special[key], 0.6); break;
+							case (config!=1 && key == '('): send_audio(audio_special[key], 0.6); break;
+							case (config!=1 && key == ')'): send_audio(audio_special[key], 0.6); break;
+							case (config!=1 && key == '['): send_audio(audio_special[key], 0.6); break;
+							case (config!=1 && key == ']'): send_audio(audio_special[key], 0.6); break;
+							case (config!=1 && key == '{'): send_audio(audio_special[key], 0.6); break;
+							case (config!=1 && key == '}'): send_audio(audio_special[key], 0.6); break;
 
-						case (config!=2 && parseInt(key) >= 1 && parseInt(key) <= 9):
-							send_audio(audio_vocals[parseInt(key)-1], 1.0);
-						break;
-						case (config!=2 && parseInt(key) == 0):
-							send_audio(audio_vocals[9], 1.0);
-						break;
-						case (config!=2 && keycode == 189):
-							send_audio(audio_vocals[10], 1.0);
-						break;
-						case (config!=2 && keycode == 187):
-							send_audio(audio_vocals[11], 1.0);
-						break;
+							case (config!=2 && parseInt(key) >= 1 && parseInt(key) <= 9):
+								send_audio(audio_vocals[parseInt(key)-1], 1.0);
+							break;
+							case (config!=2 && parseInt(key) == 0):
+								send_audio(audio_vocals[9], 1.0);
+							break;
+							case (config!=2 && keycode == 189):
+								send_audio(audio_vocals[10], 1.0);
+							break;
+							case (config!=2 && keycode == 187):
+								send_audio(audio_vocals[11], 1.0);
+							break;
 
-						case (config!=2 && key == 'OK'): send_audio(audio_ok, 0.6, 0.0, 0.0, 1, true); break;
-						case (config!=2 && (keycode >= 65 && keycode <= 90 || (keycode >= 97 && keycode <= 122))):
-							//When typing in caps have a slighty higher and louder pitch with more variation
-							if (isUpperCase(key)) send_audio(audio_animalese[keycode - 65], 0.7, 0.15, 1.6, 1, true);
-							else send_audio(audio_animalese[keycode - 97], 0.5, 0.0, 0, 1, true);
-						break;
+							case (config!=2 && key == 'OK'): send_audio(audio_ok, 0.6, 0.0, 0.0, 1, true); break;
+							case (config!=2 && (keycode >= 65 && keycode <= 90 || (keycode >= 97 && keycode <= 122))):
+								//When typing in caps have a slighty higher and louder pitch with more variation
+								if (isUpperCase(key)) send_audio(audio_animalese[keycode - 65], 0.7, 0.15, 1.6, 1, true);
+								else send_audio(audio_animalese[keycode - 97], 0.5, 0.0, 0, 1, true);
+							break;
 
-						default:
-							//Default sound to play
-							send_audio(config!=1 && audio_special["default"], 0.4, 0.4);
-						break;
+							default:
+								//Default sound to play
+								send_audio(config!=1 && audio_special["default"], 0.4, 0.4);
+							break;
+						}
 					}
 				}
 			break;
