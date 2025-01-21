4f0e04e9a18a3e575c935c139eb230731265c8da
Josh <125091065+joshxviii@users.noreply.github.com>
2025-01-21 17:19:09 -0500

    Fixed injecting too many scripts

--- background.js
+++ background.js
@@ -63,14 +63,6 @@ function injectAnimaleseAllTabs() {
 		}
 	});
 };
-chrome.tabs.onUpdated.addListener(function(id,changeInfo,tab){
-	if (changeInfo.url === undefined && changeInfo.status == 'complete' && tab.status == 'complete') {
-		chrome.scripting.executeScript({
-			target: {tabId: id},
-			files: chrome.runtime.getManifest().content_scripts[0].js
-		}).catch(()=>{});
-	}
-});
 let creating;
 async function hasOffscreenDocument(path) {
 	const offscreenUrl = chrome.runtime.getURL(path);
